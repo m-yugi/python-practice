baaa064a6d3ea4ff0ee3ce799cebc2a909d2a018
m-yugi <msaiyugandhar333@gmail.com>
2021-08-26 18:22:07 +0530

    my first code on palindrome

--- practice.py
+++ practice.py
@@ -1,5 +1,15 @@
 x = int(input())
-v = str(x)
-y = "".join(reversed(v))
-print(v == y)
-print("thanks")
+y = str(x)
+v = len(y)
+mid = v // 2
+e = "".join(reversed(y[0:mid + 1]))
+if v % 2 == 0 and y[0:mid + 1] == "".join(reversed(y[mid - 1:v])):
+    print(1)
+elif v == 2 and y[0] == y[1]:
+    print(1)
+elif y[0] == '-':
+    print(0)
+elif e == y[mid:v]:
+    print(1)
+else:
+    print(0)
